dfaaad55f93c697a61a695b1c3e880e6fc5e3562
webbestmaster <web.best.master@gmail.com>
2016-06-30 22:53:02 +0300

    test cover - refactoring

--- test-cover.sh
+++ test-cover.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
+reportsFolder=reports-cover
+
 clear
 
 cd ./test
 
-./../node_modules/.bin/istanbul cover _mocha test-unit
+./../node_modules/.bin/istanbul cover --root ./../ --dir $reportsFolder _mocha test-unit
+
+unamestr=`uname`
+reportPath="$reportsFolder/lcov-report/index.html"
+if [[ "$unamestr" == 'Darwin' ]]; then # detect MacOS
+   open $reportPath
+elif [[ "$unamestr" == 'Linux' ]]; then # detect Linux
+   xdg-open $reportPath
+fi
